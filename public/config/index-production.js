4e10b5a373dd4e346edc802a65d77c4923aa1414
胡小胖子的小黑球 <1327502285@qq.com>
2024-05-22 20:38:39 +0800

    大屏前端

--- public/config/index-production.js
+++ public/config/index-production.js
@@ -1,7 +1,7 @@
 window.ENV = 'production'
 var productionConfig = {
-  baseUrl: 'https://dashboard.timestone.mzdata.top/bigScreenServer',
-  fileUrlPrefix: 'https://dashboard.timestone.mzdata.top/bigScreenServer' + '/static'
+  baseUrl: 'http://dashboard.timestone.mzdata.top/bigScreenServer',
+  fileUrlPrefix: 'http://dashboard.timestone.mzdata.top/bigScreenServer' + '/static'
 
 }
 // 必须的
